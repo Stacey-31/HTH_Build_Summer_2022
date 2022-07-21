ad81705ac3799a29f1f49082294715d785a1e455
HenryBowe <>
2022-07-20 21:26:46 -0400

    added a new csv file

--- Finding-an-Interesting-Dataset/happiness_dataset.py
+++ Finding-an-Interesting-Dataset/happiness_dataset.py
@@ -3,6 +3,9 @@ import pandas
 pandas.options.display.max_columns = None
 pandas.options.display.max_rows = None
 
-csv_data = pandas.read_csv("Finding-an-Interesting-Dataset/Happiness_data.csv")
+csv_data19 = pandas.read_csv("Finding-an-Interesting-Dataset/Happiness_data_2019.csv")
 
-print(csv_data)
+csv_data21 = pandas.read_csv("Finding-an-Interesting-Dataset/Happiness_data_2021.csv")
+
+print(csv_data19)
+print(csv_data21)
